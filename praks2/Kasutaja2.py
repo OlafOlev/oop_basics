d8eef1ca15968e1f6a7d64a875479bbbc07e26ed
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-01-20 09:41:01 +0200

    Kasutaja2

---
+++ praks2/Kasutaja2.py
@@ -0,0 +1,18 @@
+from praks2.Kasutajad2 import Kasutajad
+kasutaja1 = Kasutajad("Janre", "Liiv")
+kasutaja1.maara_kasutajanimi("Janre.Liiv")
+kasutaja1.maara_parool("1111")
+kasutaja1.kirjelda_kasutaja()
+kasutaja1.tervita_kasutaja()
+
+kasutaja2 = Kasutajad("Ax-Sylvester", "Hommuk")
+kasutaja2.maara_kasutajanimi("Ax-Sylvaster.Hommuk")
+kasutaja2.maara_parool("2222")
+kasutaja2.kirjelda_kasutaja()
+kasutaja2.tervita_kasutaja()
+
+kasutaja3 = Kasutajad("Olaf Olev", "Alakivi")
+kasutaja3.maara_kasutajanimi("Olaf-Olev.Alakivi")
+kasutaja3.maara_parool("3333")
+kasutaja3.kirjelda_kasutaja()
+kasutaja3.tervita_kasutaja()

b640e7d4e5afe4152e2054dc669c3c6ac8aba716
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-01-31 12:55:24 +0200

    kasutaja3

---
+++ praks2/ylesanne10/kasutajad3.py
@@ -0,0 +1,20 @@
+class Kasutajad():
+    sisselogimiskatsed = 0
+    def __init__(self, eesnimi, perenimi):
+        self.eesnimi = eesnimi
+        self.perenimi = perenimi
+
+    def maara_kasutajanimi(self, kasutajanimi):
+        self.kasutajanimi = kasutajanimi
+    def maara_parool(self, parool):
+        self.parool = parool
+
+    def kirjelda_kasutaja(self):
+        print("Kasutaja eesnimi on {0}, perenimi on {1}, kasutajanimi on {2}, parool on  {3}".format(self.eesnimi, self.perenimi, self.kasutajanimi, self.parool))
+
+    def tervita_kasutaja(self):
+        print("Tervitan kasutajat {0}".format(self.eesnimi))
+    def suurenda_sisselogimiskatsed(self):
+        self.sisselogimiskatsed += 1
+    def reset_sisselogimiskatsed(self):
+        self.sisselogimiskatsed = 0
